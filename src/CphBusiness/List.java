19f3cbc860c4442ccbcc4319f3be4cba17c3949a
Vincent Buchholz <vincbuchholz@gmail.com>
2021-09-07 10:06:29 +0200

    Creates InsertFromHead() in List and tests it

--- src/CphBusiness/List.java
+++ src/CphBusiness/List.java
@@ -3,4 +3,17 @@ package CphBusiness;
 public class List {
     Node head = null;
     Node tail = null;
+
+    public Node insertFromHead(Node node){
+        if (head == null){
+            head = node;
+            tail = node;
+            return head;
+        }
+        else {
+            head.prev = node;
+            head = node;
+            return head;
+        }
+    }
 }
---
+++ src/CphBusiness/ListTest.java
@@ -0,0 +1,21 @@
+package CphBusiness;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ListTest {
+
+    List list = new List();
+
+    @Test
+    void insertFromHead(){
+        Node node = new Node("Vincent");
+        Node node1 = new Node("Thomas");
+
+
+        assertEquals("Vincent", list.insertFromHead(node).name);
+        assertEquals("Thomas", list.insertFromHead(node1).name);
+    }
+
+}
