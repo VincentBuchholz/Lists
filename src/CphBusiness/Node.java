91285bd868c3459f145bb16f7a23ce1bae4c0a88
Vincent Buchholz <vincbuchholz@gmail.com>
2021-09-07 09:50:28 +0200

    Creates Node class

--- src/CphBusiness/Node.java
+++ src/CphBusiness/Node.java
@@ -1,4 +1,14 @@
 package CphBusiness;
 
 public class Node {
+
+    String name;
+
+    Node next = null;
+    Node prev = null;
+
+    public Node(String name) {
+        this.name = name;
+    }
+
 }
